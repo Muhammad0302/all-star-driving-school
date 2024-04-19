67d1e1c8fd08b82605fde1c7ea9e84d710299ddd
@mi477048 <mi477048@gmail.com>
2024-04-19 17:32:23 -0300

    changes added

--- util/getBaseUrl.ts
+++ util/getBaseUrl.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = () => {
-  return 'http://localhost:7000'
-  // return 'https://all-start-driving-school-backend-production.up.railway.app'
+  // return 'http://localhost:7000'
+  return 'https://all-start-driving-school-backend-production.up.railway.app'
 }
