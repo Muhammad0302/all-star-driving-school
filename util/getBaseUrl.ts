493be6a5c4576de0061f08e53bdda461c2a6b398
@mi477048 <mi477048@gmail.com>
2024-04-25 11:39:45 -0300

    added production url

--- util/getBaseUrl.ts
+++ util/getBaseUrl.ts
@@ -1,4 +1,5 @@
 export const getBaseUrl = () => {
-  return 'http://localhost:7000'
+  // return 'http://localhost:7000'
+  return 'https://all-start-driving-school-backend.vercel.app'
   // return 'https://all-start-driving-school-backend-production.up.railway.app'
 }
