f10f7d67a62fe91217636a2623589d383c319aa9
@mi477048 <mi477048@gmail.com>
2024-05-08 13:22:12 -0300

    changes added

--- components/AddStudent/AddStudent.tsx
+++ components/AddStudent/AddStudent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as yup from 'yup'
 import { useFormik } from 'formik'
 import { Grid, TextField, Button } from '@mui/material'
@@ -13,7 +13,7 @@ import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 import Box from '@mui/material/Box'
 import FormHelperText from '@mui/material/FormHelperText'
-import { addStudent } from 'services/room'
+import { addStudent, getAllInstructors } from 'services/room'
 import dayjs from 'dayjs'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer, toast, Bounce } from 'react-toastify'
@@ -32,14 +32,22 @@ const validationSchema = yup.object({
   licenseIssueDate: yup.date().required('License Issue Date is required'),
   licenseExpiryDate: yup.date().required('License Expiry Date is required'),
   courseStartDate: yup.date().required('Course Start Date is required'),
+  no_of_lesson: yup.number().required('No of Lesson is required'),
+  road_test: yup.string().required('Road test is required'),
+  price: yup.number().required('Total price of lesson is required'),
+  instructorName: yup.string().required('Instructor is required'),
 })
 
 const AddStudent = () => {
   const router = useRouter()
-
+  const [instructors, setInstructors] = useState([])
+  const [instructorId, setInstructorId] = useState(null)
   const formik = useFormik({
     initialValues: {
       registration_for: '',
+      instructorName: '',
+      no_of_lesson: '',
+      price: '',
       firstName: '',
       lastName: '',
       email: '',
@@ -48,6 +56,7 @@ const AddStudent = () => {
       dob: null,
       gender: '',
       licenseNumber: '',
+      road_test: '',
       licenseIssueDate: null,
       licenseExpiryDate: null,
       courseStartDate: null,
@@ -67,7 +76,12 @@ const AddStudent = () => {
         licence_issue_date: values.licenseIssueDate,
         licence_expiry_date: values.licenseExpiryDate,
         course_start_date: values.courseStartDate,
+        instructor_id: instructorId,
+        no_of_lesson: parseInt(values.no_of_lesson),
+        price_per_lesson: parseInt(values.price),
+        road_test: values.road_test,
       }
+      console.log('The sending data is:', data)
       try {
         const res = await addStudent(data)
         console.log('Add Student api response', res)
@@ -106,6 +120,24 @@ const AddStudent = () => {
     // Add hyphens after the 5th and 11th characters
     return formattedValue.replace(/(.{5})(.{1,6})/, '$1-$2').replace(/(.{11})(.{1,6})/, '$1-$2')
   }
+
+  useEffect(() => {
+    const fetchInstructorData = async () => {
+      try {
+        const res = await getAllInstructors()
+        // console.log('The instructor data is:', res)
+        const instructor = res.instructors
+        setInstructors(instructor)
+      } catch (error) {
+        console.error('Error fetching instructor data:', error)
+      }
+    }
+
+    fetchInstructorData()
+  }, [])
+
+  console.log('The formik values is:', formik.values)
+
   return (
     <div className='mt-[3.5rem]'>
       <form onSubmit={formik.handleSubmit}>
@@ -129,6 +161,11 @@ const AddStudent = () => {
                 <MenuItem value='Online'>Online</MenuItem>
                 <MenuItem value='Onsite'>Onsite</MenuItem>
               </Select>
+              {formik.touched.registration_for && Boolean(formik.errors.registration_for) && (
+                <FormHelperText sx={{ color: '#d32f2f' }}>
+                  {formik.errors.registration_for}
+                </FormHelperText>
+              )}
             </FormControl>
           </Grid>
 
@@ -267,6 +304,137 @@ const AddStudent = () => {
               helperText={formik.touched.licenseNumber && (formik.errors.licenseNumber as any)}
             />
           </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id='gender-label'>Gender</InputLabel>
+              <Select
+                labelId='gender-label'
+                id='gender'
+                value={formik.values.gender}
+                onChange={(e) => formik.setFieldValue('gender', e.target.value)}
+                label='Gender'
+              >
+                <MenuItem value='male'>Male</MenuItem>
+                <MenuItem value='female'>Female</MenuItem>
+                <MenuItem value='other'>Other</MenuItem>
+              </Select>
+              {formik.touched.gender && Boolean(formik.errors.gender) && (
+                <FormHelperText sx={{ color: '#d32f2f' }}>{formik.errors.gender}</FormHelperText>
+              )}
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id='no_of_lesson'
+              name='no_of_lesson'
+              label='No of Lesson'
+              variant='outlined'
+              fullWidth
+              sx={{
+                '& fieldset': { borderColor: '#f23d4d !important' },
+              }}
+              InputLabelProps={{
+                focused: false,
+              }}
+              value={formik.values.no_of_lesson}
+              onChange={formik.handleChange}
+              error={formik.touched.no_of_lesson && Boolean(formik.errors.no_of_lesson)}
+              helperText={formik.touched.no_of_lesson && (formik.errors.no_of_lesson as any)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id='price'
+              name='price'
+              label='Total Price Of Lesson'
+              variant='outlined'
+              fullWidth
+              sx={{
+                '& fieldset': { borderColor: '#f23d4d !important' },
+              }}
+              InputLabelProps={{
+                focused: false,
+              }}
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && (formik.errors.price as any)}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id='demo-simple-select-label'
+                  error={formik.touched.road_test && Boolean(formik.errors.road_test)}
+                >
+                  Road Test
+                </InputLabel>
+                <Select
+                  labelId='demo-simple-select-label'
+                  id='demo-simple-select'
+                  value={formik.values.road_test}
+                  label='Package'
+                  onChange={(e) => {
+                    formik.setFieldValue('road_test', e.target.value)
+                  }}
+                >
+                  <MenuItem value={'yes'}>Yes</MenuItem>
+                  <MenuItem value={'no'}>No</MenuItem>
+                </Select>
+                {formik.touched.road_test && Boolean(formik.errors.road_test) && (
+                  <FormHelperText sx={{ color: '#d32f2f' }}>
+                    {formik.errors.road_test}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id='demo-simple-select-label'
+                  error={formik.touched.instructorName && Boolean(formik.errors.instructorName)}
+                >
+                  Assign Instructor
+                </InputLabel>
+                <Select
+                  labelId='demo-simple-select-label'
+                  id='demo-simple-select'
+                  value={formik.values.instructorName}
+                  label='Instructor Name'
+                  onChange={(e) => {
+                    formik.setFieldValue('instructorName', e.target.value)
+                    const [selectedFirstName, selectedLastName] = e.target.value.split(' ')
+
+                    const selectedInstructor: any = instructors.find(
+                      (instructor: any) =>
+                        instructor.firstName === selectedFirstName &&
+                        instructor.lastName === selectedLastName,
+                    )
+                    if (selectedInstructor) {
+                      setInstructorId(selectedInstructor._id)
+                    }
+                  }}
+                >
+                  {instructors?.map((instructor: any, index) => (
+                    <MenuItem key={index} value={`${instructor.firstName} ${instructor.lastName}`}>
+                      {`${instructor.firstName} ${instructor.lastName}`}
+                    </MenuItem>
+                  ))}
+                </Select>
+
+                {formik.touched.instructorName && Boolean(formik.errors.instructorName) && (
+                  <FormHelperText sx={{ color: '#d32f2f' }}>
+                    {formik.errors.instructorName}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Box>
+          </Grid>
 
           <Grid item xs={12} sm={3} style={{ marginTop: '-8px' }}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -328,22 +496,6 @@ const AddStudent = () => {
             </LocalizationProvider>
           </Grid>
 
-          <Grid item xs={12} sm={3}>
-            <FormControl fullWidth>
-              <InputLabel id='gender-label'>Gender</InputLabel>
-              <Select
-                labelId='gender-label'
-                id='gender'
-                value={formik.values.gender}
-                onChange={(e) => formik.setFieldValue('gender', e.target.value)}
-                label='Gender'
-              >
-                <MenuItem value='male'>Male</MenuItem>
-                <MenuItem value='female'>Female</MenuItem>
-                <MenuItem value='other'>Other</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
           <Grid item xs={12} container justifyContent='flex-end'>
             <Button
               type='submit'
--- components/EditStudent/EditStudent.tsx
+++ components/EditStudent/EditStudent.tsx
@@ -10,10 +10,10 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
+import Select, { SelectChangeEvent, selectClasses } from '@mui/material/Select'
 import Box from '@mui/material/Box'
 import FormHelperText from '@mui/material/FormHelperText'
-import { addStudent, getStudentById, editStudent } from 'services/room'
+import { addStudent, getStudentById, editStudent, getAllInstructors } from 'services/room'
 import dayjs from 'dayjs'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer, toast, Bounce } from 'react-toastify'
@@ -32,14 +32,25 @@ const validationSchema = yup.object({
   licenseIssueDate: yup.date().required('License Issue Date is required'),
   licenseExpiryDate: yup.date().required('License Expiry Date is required'),
   courseStartDate: yup.date().required('Course Start Date is required'),
+  no_of_lesson: yup.number().required('No of Lesson is required'),
+  road_test: yup.string().required('Road test is required'),
+  price: yup.number().required('Total price of lesson is required'),
+  instructorName: yup.string().required('Instructor is required'),
 })
 
 const EditStudent = ({ params }: any) => {
   const router = useRouter()
+  const [instructors, setInstructors] = useState([])
+  const [instructorName, setInstructorName] = useState('')
+  const [assign, setAssign] = useState(null)
+  const [instructorId, setInstructorId] = useState(null)
   const [studentData, setStudentData] = useState([])
   const formik = useFormik({
     initialValues: {
       registration_for: '',
+      instructorName: '',
+      no_of_lesson: '',
+      price: '',
       firstName: '',
       lastName: '',
       email: '',
@@ -48,6 +59,7 @@ const EditStudent = ({ params }: any) => {
       dob: null,
       gender: '',
       licenseNumber: '',
+      road_test: '',
       licenseIssueDate: null,
       licenseExpiryDate: null,
       courseStartDate: null,
@@ -106,6 +118,7 @@ const EditStudent = ({ params }: any) => {
         const res = await getStudentById(params.studentid)
         console.log('The student data is:', res)
         const student = res.student
+        const Assign = res.assign
         const parsedDob = dayjs(student.dob)
         const parselicence_issue_date = dayjs(student.licence_issue_date)
         const parselicence_expiry_date = dayjs(student.licence_expiry_date)
@@ -115,15 +128,17 @@ const EditStudent = ({ params }: any) => {
         student.lastName = student.lastName
         student.email = student.email
         student.phoneNumber = student.phone_number
-
         student.address = student.address
         student.dob = parsedDob
         student.gender = student.gender
-
         student.licenseNumber = student.licence_no
         student.licenseIssueDate = parselicence_issue_date
         student.licenseExpiryDate = parselicence_expiry_date
         student.courseStartDate = parsecourse_start_date
+        student.no_of_lesson = Assign.no_of_lesson
+        student.price = Assign.price_per_lesson
+        student.road_test = Assign.road_test
+        setInstructorId(Assign.instructor_id)
         setStudentData(student)
       } catch (error) {
         console.error('Error fetching student data:', error)
@@ -140,7 +155,44 @@ const EditStudent = ({ params }: any) => {
       formik.setValues(studentData)
     }
   }, [studentData])
-  //
+
+  useEffect(() => {
+    const fetchInstructorData = async () => {
+      try {
+        const res = await getAllInstructors()
+        // console.log('The instructor data is:', res)
+        const instructor = res.instructors
+        setInstructors(instructor)
+      } catch (error) {
+        console.error('Error fetching instructor data:', error)
+      }
+    }
+
+    fetchInstructorData()
+  }, [])
+
+  useEffect(() => {
+    if (instructors.length > 0 && instructorId) {
+      const selectedInstructor: any = instructors.find(
+        (instructor: any) => instructor._id === instructorId,
+      )
+      if (selectedInstructor) {
+        const instructor = `${selectedInstructor.firstName} ${selectedInstructor.lastName}`
+        setInstructorName(instructor)
+        formik.setFieldValue('instructorName', instructor)
+      }
+    }
+  }, [instructorId, instructors])
+
+  const formatLicenseNumber = (value: any) => {
+    // Remove any existing hyphens and limit input to 17 characters
+    const formattedValue = value.replace(/-/g, '').substr(0, 15)
+    // Add hyphens after the 5th and 11th characters
+    return formattedValue.replace(/(.{5})(.{1,6})/, '$1-$2').replace(/(.{11})(.{1,6})/, '$1-$2')
+  }
+
+  console.log('formik values is:', formik.values)
+
   return (
     <div className='mt-[3.5rem]'>
       <form onSubmit={formik.handleSubmit}>
@@ -297,10 +349,145 @@ const EditStudent = ({ params }: any) => {
                 focused: false,
               }}
               value={formik.values.licenseNumber}
-              onChange={formik.handleChange}
+              // onChange={formik.handleChange}
+              onChange={(e) => {
+                const formattedValue = formatLicenseNumber(e.target.value)
+                formik.setFieldValue('licenseNumber', formattedValue)
+              }}
               error={formik.touched.licenseNumber && Boolean(formik.errors.licenseNumber)}
               helperText={formik.touched.licenseNumber && (formik.errors.licenseNumber as any)}
             />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id='gender-label'>Gender</InputLabel>
+              <Select
+                labelId='gender-label'
+                id='gender'
+                value={formik.values.gender}
+                onChange={(e) => formik.setFieldValue('gender', e.target.value)}
+                label='Gender'
+              >
+                <MenuItem value='male'>Male</MenuItem>
+                <MenuItem value='female'>Female</MenuItem>
+                <MenuItem value='other'>Other</MenuItem>
+              </Select>
+              {formik.touched.gender && Boolean(formik.errors.gender) && (
+                <FormHelperText sx={{ color: '#d32f2f' }}>{formik.errors.gender}</FormHelperText>
+              )}
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id='no_of_lesson'
+              name='no_of_lesson'
+              label='No of Lesson'
+              variant='outlined'
+              fullWidth
+              sx={{
+                '& fieldset': { borderColor: '#f23d4d !important' },
+              }}
+              InputLabelProps={{
+                focused: false,
+              }}
+              value={formik.values.no_of_lesson}
+              onChange={formik.handleChange}
+              error={formik.touched.no_of_lesson && Boolean(formik.errors.no_of_lesson)}
+              helperText={formik.touched.no_of_lesson && (formik.errors.no_of_lesson as any)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id='price'
+              name='price'
+              label='Total Price Of Lesson'
+              variant='outlined'
+              fullWidth
+              sx={{
+                '& fieldset': { borderColor: '#f23d4d !important' },
+              }}
+              InputLabelProps={{
+                focused: false,
+              }}
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && (formik.errors.price as any)}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id='demo-simple-select-label'
+                  error={formik.touched.road_test && Boolean(formik.errors.road_test)}
+                >
+                  Road Test
+                </InputLabel>
+                <Select
+                  labelId='demo-simple-select-label'
+                  id='demo-simple-select'
+                  value={formik.values.road_test}
+                  label='Package'
+                  onChange={(e) => {
+                    formik.setFieldValue('road_test', e.target.value)
+                  }}
+                >
+                  <MenuItem value={'yes'}>Yes</MenuItem>
+                  <MenuItem value={'no'}>No</MenuItem>
+                </Select>
+                {formik.touched.road_test && Boolean(formik.errors.road_test) && (
+                  <FormHelperText sx={{ color: '#d32f2f' }}>
+                    {formik.errors.road_test}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id='demo-simple-select-label'
+                  error={formik.touched.instructorName && Boolean(formik.errors.instructorName)}
+                >
+                  Assign Instructor
+                </InputLabel>
+                <Select
+                  labelId='demo-simple-select-label'
+                  id='demo-simple-select'
+                  value={formik.values.instructorName}
+                  label='Instructor Name'
+                  onChange={(e) => {
+                    formik.setFieldValue('instructorName', e.target.value)
+                    const [selectedFirstName, selectedLastName] = e.target.value.split(' ')
+
+                    const selectedInstructor: any = instructors.find(
+                      (instructor: any) =>
+                        instructor.firstName === selectedFirstName &&
+                        instructor.lastName === selectedLastName,
+                    )
+                    if (selectedInstructor) {
+                      setInstructorId(selectedInstructor._id)
+                    }
+                  }}
+                >
+                  {instructors?.map((instructor: any, index) => (
+                    <MenuItem key={index} value={`${instructor.firstName} ${instructor.lastName}`}>
+                      {`${instructor.firstName} ${instructor.lastName}`}
+                    </MenuItem>
+                  ))}
+                </Select>
+
+                {formik.touched.instructorName && Boolean(formik.errors.instructorName) && (
+                  <FormHelperText sx={{ color: '#d32f2f' }}>
+                    {formik.errors.instructorName}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Box>
           </Grid>
 
           <Grid item xs={12} sm={3} style={{ marginTop: '-8px' }}>
@@ -363,22 +550,6 @@ const EditStudent = ({ params }: any) => {
             </LocalizationProvider>
           </Grid>
 
-          <Grid item xs={12} sm={3}>
-            <FormControl fullWidth>
-              <InputLabel id='gender-label'>Gender</InputLabel>
-              <Select
-                labelId='gender-label'
-                id='gender'
-                value={formik.values.gender}
-                onChange={(e) => formik.setFieldValue('gender', e.target.value)}
-                label='Gender'
-              >
-                <MenuItem value='male'>Male</MenuItem>
-                <MenuItem value='female'>Female</MenuItem>
-                <MenuItem value='other'>Other</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
           <Grid item xs={12} container justifyContent='flex-end'>
             <Button
               type='submit'
