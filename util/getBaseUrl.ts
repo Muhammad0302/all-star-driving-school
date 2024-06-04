f5f3c5c6faa03b6d7a707dadc3d2507c5abaa52d
@mi477048 <mi477048@gmail.com>
2024-06-04 11:32:23 -0300

    changes added

--- util/getBaseUrl.ts
+++ util/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
   // return 'http://localhost:7000'
-  return 'https://allstar-mocha.vercel.app'
+  return 'https://newallstar.vercel.app'
   // return 'https://all-start-driving-school-backend-production.up.railway.app'
 }
