729aff540eed7398551e2627d45727601ad0c54a
@mi477048 <mi477048@gmail.com>
2024-06-24 12:28:19 -0300

    changes added

--- util/getBaseUrl.ts
+++ util/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
-  return 'http://localhost:7000'
-  // return 'https://drivingbackend-omega.vercel.app'
+  // return 'http://localhost:7000'
+  return 'https://drivingbackend-omega.vercel.app'
   // return 'https://all-start-driving-school-backend-production.up.railway.app'
 }
