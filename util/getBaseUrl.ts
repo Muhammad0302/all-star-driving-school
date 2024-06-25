35fae253f8b8d00e660b51ee35fa286da578f890
@mi477048 <mi477048@gmail.com>
2024-06-25 10:24:02 -0300

    changes added

--- util/getBaseUrl.ts
+++ util/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
-  return 'http://localhost:7000'
-  // return 'https://drivingbackend-omega.vercel.app'
+  // return 'http://localhost:7000'
+  return 'https://all-start-driving-school-backend.vercel.app'
   // return 'https://backend1-olive.vercel.app'
 }
