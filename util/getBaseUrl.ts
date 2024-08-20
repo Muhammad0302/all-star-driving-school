44b3b04b0cb76252e59ab3e05f626e15534f9175
Muhammad Ismail <60970076+Muhammad0302@users.noreply.github.com>
2024-08-20 16:51:01 -0300

    Update getBaseUrl.ts

--- util/getBaseUrl.ts
+++ util/getBaseUrl.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = () => {
   // return 'http://localhost:7000'
-  return 'https://all-start-driving-school-backend.vercel.app'
+  return 'https://all-start-driving-school-backend.vercel.app-baseurl'
 }
